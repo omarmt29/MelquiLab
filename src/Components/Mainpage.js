e5e6a5db377cf1cc2a79b1e95e508a45a4b79072
omarmt29 <omarmendezt29@gmail.com>
2022-04-28 00:36:59 -0400

    el primer commit en omar

---
+++ src/Components/Mainpage.js
@@ -0,0 +1,39 @@
+import main from '../Main.css'   // import main.css
+import logo from '../source/img/Adequi2.png'
+
+export default function Mainpage() {
+    return (
+        <>
+            <div className="Main-container">
+
+               {/* LOGO */}
+               <div className='Logo-container'>
+                   <img src={logo}/>
+               </div>
+
+               {/* NAVBAR */}
+               
+
+               {/* SLIDER A LA IZQUIERDA */}
+
+               {/* ASIDE DE NOTICIAS LA DERECHA */}
+
+               {/* DIRECTIVA SLIDER */}
+
+               {/* FOOTER */}
+                
+
+
+
+
+
+
+
+
+            </div>
+
+
+        </>
+
+    )
+};
